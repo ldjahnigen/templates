a8fcab3daee6c3ad022357e2f4854830e0bde219
Louis <ldjahnigen@gmail.com>
2023-11-28 11:40:25 -0500

    intial function structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+template <typename T>
+T half(T x);
+
 int main() {
 
   return 0;
+}
+
+
+double half(double x) {
+  return x / 2;
+}
+
+
+float half(float x) {
+  return x / 2;
+}
+
+
+int half(int x) {
+
 }
